190d9182145d976d84b3a39cef3ae94aff8f8437
naumshubaev <90708953+naumshubaev@users.noreply.github.com>
2022-03-13 20:42:18 +0300

    OrderSuccess set to pass

--- src/test/java/ru/netology/OrderSuccess.java
+++ src/test/java/ru/netology/OrderSuccess.java
@@ -58,7 +58,7 @@ public class OrderSuccess {
 
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']"))
                 .getText().trim();
-        String expected = "demoFAIL";
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
 
         assertEquals(expected, actual);
 
